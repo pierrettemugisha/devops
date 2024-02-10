9d74daeacd1b29ae9bfaa1e13168fe4af2b327eb
Pierrette Mugisha <mupierrix@yahoo.fr>
2024-02-10 18:54:49 -0500

    devop-module 6

--- hunam-gov-infrastructure/terraform/modules/aws_humangov_infrastructure/main.tf
+++ hunam-gov-infrastructure/terraform/modules/aws_humangov_infrastructure/main.tf
@@ -6,42 +6,6 @@
 #############################################################################
 
 
-# Resource to create security group
-resource "aws_security_group" "state_ec2_sg" {
-  name        = "humangov-${var.state_name}-ec2-sg"
-  description = "Allow traffic on ports 22 and 80"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "humangov-${var.state_name}"
-  }
-}
-
-# Resource to create an EC2 instance
-resource "aws_instance" "state_ec2" {
-  ami                    = "ami-007855ac798b5175e"
-  instance_type          = "t2.micro"
-  key_name               = "humangov-ec2-key"
-  vpc_security_group_ids = [aws_security_group.state_ec2_sg.id]
-
-  tags = {
-    Name = "humangov-${var.state_name}"
-  }
-}
-
 #Resource to create a DynamoDB
 resource "aws_dynamodb_table" "state_dynamodb" {
   name         = "humangov-${var.state_name}-dynamodb"

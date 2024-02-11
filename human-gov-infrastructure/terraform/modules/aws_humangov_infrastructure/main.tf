bcdfc5cd3d888f4c602036668d20afda522da41e
Pierrette Mugisha <mupierrix@yahoo.fr>
2024-02-10 19:02:32 -0500

    Directory name typo

--- human-gov-infrastructure/terraform/modules/aws_humangov_infrastructure/main.tf
+++ human-gov-infrastructure/terraform/modules/aws_humangov_infrastructure/main.tf
@@ -6,7 +6,6 @@
 #############################################################################
 
 
-#Resource to create a DynamoDB
 resource "aws_dynamodb_table" "state_dynamodb" {
   name         = "humangov-${var.state_name}-dynamodb"
   billing_mode = "PAY_PER_REQUEST"
@@ -22,15 +21,12 @@ resource "aws_dynamodb_table" "state_dynamodb" {
   }
 }
 
-# Resource to create a random string.
-# This will be used to generate a unique S3 name, as S3 names are unique worldwide.
 resource "random_string" "bucket_suffix" {
   length  = 4
   special = false
   upper   = false
 }
 
-# Resource to create an S3 bucket
 resource "aws_s3_bucket" "state_s3" {
   bucket = "humangov-${var.state_name}-s3-${random_string.bucket_suffix.result}"
   acl    = "private"
